7866865a01ba7e8dc9d62f95be1f91247be5d0a3
John Vizuet <jvizuetr@gmail.com>
2025-04-18 16:18:12 -0600

    Limpieza de código con Flake8, Black, SOLID, linters, mypy

--- app/database.py
+++ app/database.py
@@ -1,17 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session, DeclarativeBase
+from typing import Generator
 
-DATABASE_URL = "mssql+pyodbc://sa:Ordico2024!@localhost:1433/OrdicoRetailInventory?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
+DATABASE_URL = (
+    "mssql+pyodbc://sa:Ordico2024!@localhost:1433/OrdicoRetailInventory"
+    "?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
+)
 
 engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
 class Base(DeclarativeBase):
     pass
 
-def get_db() -> Session:
-    db =  SessionLocal()
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
     try:
         yield db
     finally:
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, Float
 from app.database import Base
 
+
 class Product(Base):
     __tablename__ = "products"
 
@@ -8,4 +9,3 @@ class Product(Base):
     name = Column(String, nullable=False)
     price = Column(Float, nullable=False)
     stock = Column(Integer, nullable=False)
-
